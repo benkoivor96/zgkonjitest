2d155bb7ee13ed95a9dcf70617a64b9d7a81ae83
benkoivor96 <benko.ivor96@gmail.com>
2026-04-30 17:10:17 +0200

    Delete functions directory

--- functions/auth.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  const clientId = context.env.GITHUB_CLIENT_ID;
-  const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=repo,user`;
-  return Response.redirect(url, 302);
-}
